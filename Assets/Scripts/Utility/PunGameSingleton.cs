368db73d6c0fab44b92bbb7ac21e8115e9e8eb86
Francis <francis-r-emond@hotmail.com>
2016-05-13 20:19:58 -0400

    photon network

---
+++ Assets/Scripts/Utility/PunGameSingleton.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+
+public class PunGameSingleton<T> : PunBehaviour where T : PunGameSingleton<T>
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType(typeof(T)) as T;
+
+                if (_instance == null)
+                {
+                    GameObject gameObject = new GameObject(typeof(T).Name);
+
+                    _instance = gameObject.AddComponent(typeof(T)) as T;
+                }
+            }
+
+            return _instance;
+        }
+    }
+
+    protected virtual void OnDestroy()
+    {
+        if (_instance)
+            Destroy(_instance);
+
+        _instance = null;
+    }
+
+    public void Resolve()
+    {
+        // Do nothing, this will force the object to be instantiated
+    }
+}
