a01b1c0bc0c107e8e7149fcf6f8f3da7fde200db
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-14 09:16:46 -0400

    need to plug ui in

--- Assets/Scripts/UI/ResultsState.cs
+++ Assets/Scripts/UI/ResultsState.cs
@@ -1,15 +1,21 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
-public class ResultsState : MonoBehaviour {
+public class ResultsState : MonoBehaviour
+{
+    public Image luckImage;
+    public Image badLuckImage;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private bool m_winningTeamIsLuck;
+    public void SetWinningTeam(bool luckWins)
+    {
+        m_winningTeamIsLuck = luckWins;
+    }
+
+    void Update()
+    {
+        luckImage.enabled = m_winningTeamIsLuck;
+        badLuckImage.enabled = !m_winningTeamIsLuck;
+    }
 }
