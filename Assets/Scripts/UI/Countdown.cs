68db5bc9888839a2da8f30b04ae5fd395fdd80d9
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-13 23:51:20 -0400

    working on in game ui

---
+++ Assets/Scripts/UI/Countdown.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Countdown : MonoBehaviour
+{
+    public float duration = 300.0f;
+
+    private Text m_text;
+ 
+     void Awake()
+    {
+        m_text = GetComponent<Text>();
+    }
+    
+     void Update()
+    {
+        duration -= Time.deltaTime;
+        //m_text.text = Mathf.Abs(duration);
+        if (duration < 0)
+        {
+            //Application.LoadLevel("gameOver");
+        }
+    }
+}
---
+++ Assets/Scripts/UI/CountdownState.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CountdownState : MonoBehaviour {
+
+  
+
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
