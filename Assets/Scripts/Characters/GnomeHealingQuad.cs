89c021f507c5dd3c0ef80dcb194f785c6c248a7e
Francis <francis-r-emond@hotmail.com>
2016-05-14 10:57:20 -0400

    mettons que vous pullez

--- Assets/Scripts/Characters/GnomeHealingQuad.cs
+++ Assets/Scripts/Characters/GnomeHealingQuad.cs
@@ -4,12 +4,11 @@ public class GnomeHealingQuad : MonoBehaviour
 {
     public float RotationSpeed;
     private float m_angle;
-    private float m_healRate;
+    [HideInInspector] public float HealRate;
 
-    void Start()
+    void Awake()
     {
         m_angle = 0;
-        m_healRate = GetComponentInParent<Gnome>().GoldBagHealRate;
     }
 
     void Update()
@@ -23,6 +22,9 @@ public class GnomeHealingQuad : MonoBehaviour
     void OnTriggerStay(Collider collider)
     {
         Character character = collider.GetComponent<Character>();
-        INetwork.Instance.RPC(character.gameObject, "TakeDamage", PhotonTargets.All, GetComponentInParent<Gnome>().GoldBagHealRate * Time.deltaTime);
+        if (character != null)
+        {
+            INetwork.Instance.RPC(character.gameObject, "TakeDamage", PhotonTargets.All, HealRate * Time.deltaTime);
+        }
     }
 }
