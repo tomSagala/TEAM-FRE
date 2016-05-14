ff17de749c0bd094e296689f05a170ed22ce091a
SpookyDo <sauce@sauce.com>
2016-05-13 22:00:45 -0400

    shooting and characther basic controls

---
+++ Assets/Scripts/Characters/Projectile.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+    [SerializeField] float m_damage;
+    [SerializeField] TeamsEnum m_firedBy;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void SetDamage(float damage)
+    {
+        m_damage = damage;
+    }
+
+    void SetFiredBy(TeamsEnum firedBy)
+    {
+        m_firedBy = firedBy;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (Helpers.CheckObjectTag(collision.gameObject, "Player") && collision.gameObject.GetComponent<Character>().GetTeam() != m_firedBy)
+        {
+            collision.gameObject.GetComponent<Character>().TakeDamage(m_damage);
+        }
+    }
+}
