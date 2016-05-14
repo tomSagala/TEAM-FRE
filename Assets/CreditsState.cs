3b756cb9c759667fd0f6ce109e8b56e4e380c525
Pascal Asselin <pascal.asselin@polymtl.ca>
2016-05-13 23:04:51 -0400

    Main menu part 2

---
+++ Assets/CreditsState.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsState : MonoBehaviour
+{
+    public void BackButton()
+    {
+        StateManager.Instance.GoToState("MainMenu");
+    }
+}
+
---
+++ Assets/InstructionsState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionsState : MonoBehaviour
+{
+    public void BackButton()
+    {
+        StateManager.Instance.GoToState("MainMenu");
+    }
+}
