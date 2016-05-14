c12c4a603101edc9b534500fad226320d631a53f
Francis <francis-r-emond@hotmail.com>
2016-05-14 00:30:40 -0400

    network spawn

---
+++ Assets/Scripts/GameBootstrap.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+public class GameBootstrap : MonoBehaviour
+{
+    public List<GameObject> CharacterPrefabs;
+    public List<Transform> CharacterSpawnPoints;
+
+	void Start () 
+    {
+        INetwork network = INetwork.Instance;
+        int characterId = network.GetCharacterId();
+        if (!network.IsConnected || characterId == -1)
+        {
+            SceneManager.LoadScene("MainMenu");
+            return;
+        }
+
+        network.Instantiate(CharacterPrefabs[characterId], CharacterSpawnPoints[characterId].position, CharacterSpawnPoints[characterId].rotation);
+	}
+}
