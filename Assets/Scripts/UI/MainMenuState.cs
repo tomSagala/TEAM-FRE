9d334467011c5ad9e9e55f59310bf1cabbc9d88a
SpookyDo <nic007nc@gmail.com>
2016-05-13 21:09:56 -0400

    added ui scripts

---
+++ Assets/Scripts/UI/MainMenuState.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MainMenuState : MonoBehaviour
+{	
+	void Update ()
+    {
+        if (Input.GetKey("escape"))
+        {
+            QuitButton();
+        }
+	}
+
+    public void QuitButton()
+    {
+        Application.Quit();
+        Debug.Log("Quit Button pushed!");
+    }
+
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
+
+    public void CreditsButton()
+    {
+        StateManager.Instance.GoToState("Credits");
+    }
+
+    public void InstructionsButton()
+    {
+        StateManager.Instance.GoToState("Instructions");
+    }
+}
