65a15ce319f42b2691d136ff77ba347f9755b28f
Francis <francis-r-emond@hotmail.com>
2016-05-15 00:27:11 -0400

    pointer

--- Assets/Scripts/UI/Pointer.cs
+++ Assets/Scripts/UI/Pointer.cs
@@ -6,10 +6,22 @@ public class Pointer : MonoBehaviour
     public float position;
     public float size;
 
+    private PlayState m_playState;
+
+    void Awake()
+    {
+        m_playState = FindObjectOfType<PlayState>();
+    }
+
 	void Update ()
     {
         Vector3 currentPos = GetComponent<RectTransform>().localPosition;
-        float newX = (size * position * -0.5f);
+        float newX = (size * BenTraductor(m_playState.LuckBadLuckRatio) * -0.5f);
         GetComponent<RectTransform>().localPosition = new Vector3(newX, currentPos.y, currentPos.z);
+    }
+
+    private float BenTraductor(float input)
+    {
+        return -1 * input;
     }
 }
